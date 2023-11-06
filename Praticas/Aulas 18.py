5564042aac951ec4c83128316871354daf9d0bd8
goncasfausto04 <20231721@novaims.unl.pt>
2023-11-06 11:26:14 +0000

    added hello world

--- Praticas/Aulas 18.py
+++ Praticas/Aulas 18.py
@@ -22,3 +22,33 @@ for i in matrix:
     counterd += 1
 
 print(listd)
+
+#exercicio 2
+
+sizelist = int(input("How many items?:"))
+
+list1 = [int(input("Input:")) for i in range(sizelist)]
+
+option = input("1, 2 or 3?:")
+
+if option == "1":
+    list2 = [i**2 for i in list1]
+    print(f"The list squared is: {list2}")
+elif option == "2":
+    matrixfim = []
+    matrixfim.append(list1)
+    listdouble = [i * 2 for i in list1]
+    matrixfim.append(listdouble)
+    listtriple = [i * 3 for i in list1]
+    matrixfim.append(listtriple)
+    print(matrixfim)
+elif option == "3":
+    sizelist2 = int(input("How many items?:"))
+
+    list2 = [int(input("Input:")) for i in range(sizelist2)]
+    matrix3 = []
+    matrix3.append(list1)
+    matrix3.append(list2)
+    print(matrix3)
+else:
+    print("Wrong input.")

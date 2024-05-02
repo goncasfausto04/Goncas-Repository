44a276ed054b41a149fa348cf8eee27ece77c9f3
goncasfausto04 <20231721@novaims.unl.pt>
2024-05-02 17:06:12 +0100

    modified:   .idea/Computation1.iml 	modified:   Computation2/Projecto G21/project_computation2.ipynb 	new file:   Goncas-Repository

--- Praticas/exs.py
+++ Praticas/exs.py
@@ -1,1 +1,19 @@
+def f1(M):
+    x = 0
+    for i in M:
+        x = x  + f2(i)
+        return x
+
+def f2(L):
+    x = L[0]
+    for i in range (1, len(L)):
+        if L[i] > x:
+            x = L[i]
+    return x
+
+k = [[4,3,2,1],[8,5,1,4]]
+print(f1(k))
+
+for i in k:
+    print(i)
 print(False or False)
